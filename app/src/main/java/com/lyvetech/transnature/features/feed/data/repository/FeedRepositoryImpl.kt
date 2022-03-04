a728df2147d8962ad680997f8b1479efa5c79274
Serdar Parahatgeldiyev <serdar.2.parahatgeldiyev@conti-engineering.com>
2022-03-04 14:25:51 +0200

    Caching remote data into local

--- app/src/main/java/com/lyvetech/transnature/features/feed/data/repository/FeedRepositoryImpl.kt
+++ app/src/main/java/com/lyvetech/transnature/features/feed/data/repository/FeedRepositoryImpl.kt
@@ -6,16 +6,44 @@ import com.lyvetech.transnature.features.feed.data.remote.FeedApiService
 import com.lyvetech.transnature.features.feed.domain.model.Trail
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
+import java.io.IOException
 
 class FeedRepositoryImpl(
     private val api: FeedApiService,
     private val dao: FeedDao
 ) : FeedRepository {
     override fun getSearchedTrails(name: String): Flow<Resource<List<Trail>>> = flow {
+        emit(Resource.Loading())
 
+        val trails = dao.getSearchedTrails(name).map { it.toTrail() }
+        emit(Resource.Loading(data = trails))
+
+        try {
+            val remoteTrails = api.getSearchedTrails(name)
+            remoteTrails.map { it.name }.let { dao.deleteTrails(it) }
+            dao.insertTrails(remoteTrails.map { it.toTrailEntity() })
+        } catch (e: HttpException) {
+            emit(
+                Resource.Error(
+                    message = "Oops, something went wrong!",
+                    data = trails
+                )
+            )
+        } catch (e: IOException) {
+            emit(
+                Resource.Error(
+                    message = "Couldn't reach server, check your internet connection.",
+                    data = trails
+                )
+            )
+        }
+
+        val newTrails = dao.getSearchedTrails(name).map { it.toTrail() }
+        emit(Resource.Success(newTrails))
     }
 
     override fun getAllTrails(): Flow<Resource<List<Trail>>> {
-
+        TODO("Not yet implemented")
     }
 }
